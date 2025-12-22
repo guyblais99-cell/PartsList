dc1a91cb3c38a0d29e72bf750d27509e9d0cc216
guyblais99-cell <guyblais.99@gmail.com>
2025-12-21 18:21:16 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'parts-tracker-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  // Add paths to any images or CSS files here if they are separate
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercept network requests to serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
